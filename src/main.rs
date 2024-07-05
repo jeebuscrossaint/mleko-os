d7e5fb86c77b48093eb94dbe410ee6e38309ea45
jeebuscrossaint <thediamond270@gmail.com>
2024-07-05 19:23:03 -0400

    colors enum, color code struct, screenchar, buffers and writers

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
