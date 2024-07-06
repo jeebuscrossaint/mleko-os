98ac939a73622194e16afa90c47717f6c18481c5
jeebuscrossaint <thediamond270@gmail.com>
2024-07-05 22:08:52 -0400

    testing implementation

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
-
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 
 // from old vga implementation
 // static VGA_BUF: &[u8] = b"Hello, world!";
-
-
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -41,6 +42,31 @@ pub extern "C" fn _start() -> ! {
     */
 
     println!("Hello World{}", "!");
+    //example panic to test panic handler
+    //panic!("Some panic message");
 
+    #[cfg(test)]
+    test_main();
+    
     loop {}
+}
+
+
+// DO NOTE THAT IN CARGO AS OF TO MY KNOWLEDGE 2019 AND EVEN NOW IN 2024 
+// THERE IS A BUG IN CARGO WHERE THERE ARE "duplicate lang item" ERRORS 
+// ON CARGO TEST IN SOME CASES. TO FIX REMOVE/COMMENT OUT THE ' panic = "abort" ' 
+// FOR A PROFILE IN THE CARGO.TOML FILE
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
