670619e1618f60bca4ea2bb8612a06a3a2dd0ea9
jeebuscrossaint <thediamond270@gmail.com>
2024-07-06 00:18:36 -0400

    Proper Qemu Test Exits

--- src/main.rs
+++ src/main.rs
@@ -57,11 +57,13 @@ pub extern "C" fn _start() -> ! {
 // ON CARGO TEST IN SOME CASES. TO FIX REMOVE/COMMENT OUT THE ' panic = "abort" ' 
 // FOR A PROFILE IN THE CARGO.TOML FILE
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Fn()]) {
     println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+    //new
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
@@ -69,4 +71,20 @@ fn trivial_assertion() {
     print!("trivial assertion... ");
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -148,7 +148,7 @@ use lazy_static::lazy_static;
 lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        color_code: ColorCode::new(Color::White, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
